eebc1038cbace7a2748bef697321559f8d987fe8
sevda-sevda <sevdaboz544@gmail.com>
2023-04-17 12:27:43 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+  return (
+    <div
+      className="d-flex justify-content-center w-100"
+      style={{ paddingTop: "15vh" }}
+    >
+      <div className="d-flex align-items-center flex-column w-75">
+        <h1>Kayıt Ekle</h1>
+        <TodoInput setTodoList={setTodoList} />
+
+        <div className="d-flex mt-3 flex-column w-75 gap-3">
+          {todoList.map((todoItem) => (
+            <TodoItem
+              key={todoItem.id}
+              todoItem={todoItem}
+              setTodoList={setTodoList}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
